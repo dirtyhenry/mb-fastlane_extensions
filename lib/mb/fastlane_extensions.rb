a7372fba3f39b8abae8bffb1ca9095c40d297aa2
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2020-10-28 11:25:35 +0100

    chore: apply Rubocop auto-fixes and complete the Gemspec file

--- lib/mb/fastlane_extensions.rb
+++ lib/mb/fastlane_extensions.rb
@@ -1,6 +1,9 @@
-require "mb/fastlane_extensions/version"
+# frozen_string_literal: true
+
+require 'mb/fastlane_extensions/version'
 
 module Mb
+  # Extensions to Fastlane as needed by MB.
   module FastlaneExtensions
     class Error < StandardError; end
     # Your code goes here...
