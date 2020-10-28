12ee56f5285960c04dae181aa276b700bba2072b
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2020-10-28 10:37:24 +0100

    feat: initial commit
    As created by bundle gem mb-fastlane_extensions --no-exe --mit --test=rspec

---
+++ lib/mb/fastlane_extensions.rb
@@ -0,0 +1,8 @@
+require "mb/fastlane_extensions/version"
+
+module Mb
+  module FastlaneExtensions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/mb/fastlane_extensions/version.rb
@@ -0,0 +1,5 @@
+module Mb
+  module FastlaneExtensions
+    VERSION = "0.1.0"
+  end
+end
